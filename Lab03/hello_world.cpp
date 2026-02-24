1c57915cb266c252ad02b95a236cb176110a1037
Ryan Smith <rysmith2113@gmail.com>
2026-02-24 17:15:43 -0500

    Add Lab03 demo code

---
+++ Lab03/hello_world.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file 		hello_world.cpp
+ * @author 		Ryan Smith (smithrm23@juniata)
+ * @brief 		Hello World in C++
+ * @date 		2026-01-25
+ * @copyright 	Copyright (c) 2026
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
